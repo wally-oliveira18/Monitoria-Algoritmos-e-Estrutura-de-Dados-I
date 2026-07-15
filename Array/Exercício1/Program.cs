d93d0de6fc5d5584ea1bd0cef4fb4fa5172bfc0c
Wallison Oliveira <wallison.victor2@gmail.com>
2026-07-15 19:01:46 -0300

    Add files via upload

---
+++ Array/Exercício1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace E1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double[] num = new double[10]; 
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Preencha o {i + 1}° elemento do vetor:");
+            num[i] = double.Parse(Console.ReadLine()); 
+        }
+
+        Console.WriteLine("Os elementos do vetor são: ");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(num[i]);
+        }
+
+    }
+}
